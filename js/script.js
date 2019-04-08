aed50c4032412bc15da14be52e4eb66d76d0dee6
Eric Richard <erichard@hubspot.com>
2019-04-09 00:19:30 +0200

    initial implementation of project

--- js/script.js
+++ js/script.js
@@ -6,26 +6,46 @@ project 1 - A Random Quote Generator
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+// Quotes to randomly iterate through
+var quotes = [
+	      { 
+		  quote: "All in all it's just another brick in the wall.",
+		  source: "Pink Floyd",
+		  citation: "Another Brick in the Wall",
+		  year: 1979 
+	      },
+	      { 
+		  quote: "What's love got to do, got to do with it?",
+		  source: "Tina Turner" 
+	      },
+	      { 
+		  quote: "Is this the real life? Is this just fantasy?",
+		  source: "Queen",
+		  citation: "Bohemian Rhapsody" 
+	      },
+	      { 
+		  quote: "I am the egg man. They are the egg men. I am the walrus. Goo goo g'joob.",
+		  source: "The Beatles" 
+	      },
+	      { 
+		  quote: "She wore a Raspberry beret. The kind you find in a second hand store.",
+		  source: "Prince and the Revolution",
+		  citation: "Raspberry Beret",
+		  year: 1985 
+	      } 
+ ];
 
+console.log( quotes );
 
+// Grab a random quote from quotes and return it.
+function getRandomQuote( ) {
+    // Figure out which quote to return
+    var rand = Math.floor( ( Math.random() * quotes.length ) );
+    var quote = quotes[ rand ];
 
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
-
-
+    return( quote );
+}
+console.log(getRandomQuote());
 
 /***
   Create the `printQuote` function to: 
@@ -40,7 +60,29 @@ project 1 - A Random Quote Generator
    - Set the `innerHTML` of the `quote-box` div to the HTML string. 
 ***/
 
+function printQuote( ) {
+    // Grab a quote
+    var quoteObject = getRandomQuote();
+    console.log( "Random quote: " + quoteObject );
 
+    var html = "";
+    html += '<p class="quote">' + quoteObject.quote + '</p>\n';
+    html += '<p class="source">' + quoteObject.source;
+
+    if ( quoteObject.citation ) {
+	html += '<span class="citation">' + quoteObject.citation + '</span>';
+    }
+
+    if ( quoteObject.year ) {
+	html += '<span class="year">' + quoteObject.year + '</span>';
+    }
+    html += "</p>\n";
+
+    console.log( html );
+
+    var domElement = document.getElementById('quote-box');
+    domElement.innerHTML = html;
+}
 
 
 /***
@@ -53,4 +95,3 @@ project 1 - A Random Quote Generator
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
